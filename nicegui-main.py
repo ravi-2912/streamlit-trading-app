5e91df43d109be91cb59135fe078f7816822c960
Ravinder Singh <ravi_29_12@hotmail.com>
2025-06-27 23:10:42 +0100

    nice gui

--- nicegui-main.py
+++ nicegui-main.py
@@ -30,18 +30,32 @@ from nicequi.router import Router
 from nicegui import ui
 
 
+
 @ui.page('/')
 @ui.page('/{_:path}')
 def main():
     router = Router()
 
-    left_drawer_minified = False
+    state = {
+        "left_drawer_left_arrow_visible": True,
+        "left_drawer_right_arrow_visible": False,
+    }
+
+    def toggle_left_drawer():
+        if left_drawer.props('mini'):
+            left_drawer.props('mini=False')
+            state['left_drawer_left_arrow_visible'] = True
+            state['left_drawer_right_arrow_visible'] = False
+        else:
+            left_drawer.props('mini=True')
+            state['left_drawer_left_arrow_visible'] = False
+            state['left_drawer_right_arrow_visible'] = True
 
     def menu_item(icon: str, label: str, action: Callable):
         with ui.item().props("clickable ripple").classes("p-5 pl-7 bg-gray-200").on('click', lambda: router.open(action)):
             with ui.item_section().props("avatar").style("min-width: 0;"):
                 ui.icon(icon)
-            ui.item_section(label).bind_visibility_from(left_drawer, 'mini', value=True)
+            ui.item_section(label).bind_visibility_from(left_drawer.props, 'mini')
 
     @router.add('/')
     def show_one():
@@ -59,42 +73,28 @@ def main():
     def show_three():
         ui.label('Content Three').classes('text-2xl')
 
-    with ui.left_drawer(top_corner=True, bottom_corner=True).classes("p-0 bg-gray-100") as left_drawer:
-
+    with ui.left_drawer(top_corner=True, bottom_corner=True).props('mini=False').classes("p-0 bg-gray-100") as left_drawer:
+        left_drawer.bind_value_to(state, 'props', lambda v: print(v))
         with ui.element('div').classes('p-0 w-full nicegui-header').style('height: 68px;'):
-
-            with ui.item().props("clickable").classes("p-5 text-3xl w-full h-full"):
+            with ui.item().props("clickable").classes("p-5 text-3xl w-full h-full").on('click', lambda: router.open(show_one)):
                 with ui.item_section().props("avatar").style("min-width: 0;"):
                     ui.icon('analytics',size='1.5em')
-                ui.item_section("Trading App").bind_visibility_from(left_drawer, 'mini', value=True)
+                ui.item_section("Trading App").bind_visibility_from(left_drawer, 'mini')
 
         with ui.list().classes("text-xl").classes("w-full justify-start items-start"):
-            # menu_item('home', 'Home', show_one)
-            # menu_item('send', 'Send', show_two)
-            # menu_item('analytics', 'Analytics', show_three)
-            with ui.item().props("clickable ripple").classes("p-5 pl-7 bg-gray-200").on('click', lambda: router.open(show_one)):
-                with ui.item_section().props("avatar").style("min-width: 0;"):
-                    ui.icon('home')
-                ui.item_section("Inbox").bind_visibility_from(left_drawer, 'mini', value=True)
-            with ui.item().props("clickable ripple").classes("p-5 pl-7 bg-gray-200").on('click', lambda: router.open(show_two)):
-                with ui.item_section().props("avatar").style("min-width: 0;"):
-                    ui.icon('send')
-                ui.item_section("Send").bind_visibility_from(left_drawer, 'mini', value=True)
-            with ui.item().props("clickable ripple").classes("p-5 pl-7 bg-gray-200").on('click', lambda: router.open(show_three)):
-                with ui.item_section().props("avatar").style("min-width: 0;"):
-                    ui.icon('send')
-                ui.item_section("Send").bind_visibility_from(left_drawer, 'mini', value=True)
+            menu_item('home', 'Home', show_one)
+            menu_item('send', 'Send', show_two)
+            menu_item('analytics', 'Analytics', show_three)
 
     with ui.right_drawer(top_corner=True, bottom_corner=True).style('background-color: #ebf1fa').props('bordered') as right_drawer:
         ui.label('RIGHT DRAWER')
 
     with ui.header().classes('items-center justify-between bg-transparent'):
         with ui.element('div').classes('flex items-center gap-2'):
-            ui.button(on_click=lambda: left_drawer.props('mini'), icon='keyboard_double_arrow_left').props('flat').classes("text-black").bind_visibility_from(left_drawer, 'mini', value=False)
-            ui.button(on_click=lambda: left_drawer.props('mini=False'), icon='keyboard_double_arrow_right').props('flat').classes("text-black").bind_visibility_from(left_drawer, 'mini', value=True)
-
-            ui.label('HEADER')
-            ui.label(f"{left_drawer.VALUE_PROP}").classes('text-sm text-gray-500')
+            ui.button(on_click=lambda: toggle_left_drawer, icon='keyboard_double_arrow_left').props('flat').classes("text-black").bind_visibility_from(left_drawer.props, 'mini')
+            ui.button(on_click=lambda: toggle_left_drawer, icon='keyboard_double_arrow_right').props('flat').classes("text-black").bind_visibility_from(left_drawer.props, 'mini')
+            print(left_drawer.props)
+            ui.label(f"{state['props']}").classes('text-lg font-bold text-black')
 
 
 
