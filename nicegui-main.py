40bb3134e214cf8dae4ae3b73271f5c42f4236d3
Ravinder Singh <ravi_29_12@hotmail.com>
2025-06-28 01:53:28 +0100

    wip

--- nicegui-main.py
+++ nicegui-main.py
@@ -1,104 +1,160 @@
-# from nicegui import ui
-
-# @ui.page('/page_layout')
-# def page_layout():
-#     ui.label('CONTENT')
-#     [ui.label(f'Line {i}') for i in range(100)]
-#     with ui.header(elevated=True).style('background-color: #3874c8').classes('items-center justify-between'):
-#         ui.label('HEADER')
-#         ui.button(on_click=lambda: left_drawer.toggle(), icon='menu').props('flat color=red')
-#         ui.button(on_click=lambda: right_drawer.toggle(), icon='menu').props('flat color=white')
-#     with ui.left_drawer(top_corner=True, bottom_corner=True, fixed=True).style('background-color: #d7e3f4') as left_drawer:
-#         ui.label('LEFT DRAWER')
-#     with ui.right_drawer(fixed=False).style('background-color: #ebf1fa').props('bordered') as right_drawer:
-#         ui.label('RIGHT DRAWER')
-#     with ui.footer().style('background-color: #3874c8'):
-#         ui.label('FOOTER')
-
-# ui.link('show page with fancy layout', page_layout)
-
-# ui.run(port=8123)
-
 import os
 import sys
 from typing import Callable
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from nicequi.router import Router
 
 from nicegui import ui
 
 
-
-@ui.page('/')
-@ui.page('/{_:path}')
+@ui.page("/")
+@ui.page("/{_:path}")
 def main():
     router = Router()
 
     state = {
         "left_drawer_left_arrow_visible": True,
         "left_drawer_right_arrow_visible": False,
+        "active_page": "home",
     }
 
-    def toggle_left_drawer():
-        if left_drawer.props('mini'):
-            left_drawer.props('mini=False')
-            state['left_drawer_left_arrow_visible'] = True
-            state['left_drawer_right_arrow_visible'] = False
-        else:
-            left_drawer.props('mini=True')
-            state['left_drawer_left_arrow_visible'] = False
-            state['left_drawer_right_arrow_visible'] = True
+    def left_drawer_collapse():
+        left_drawer.props("mini")
+        state["left_drawer_left_arrow_visible"] = False
+        state["left_drawer_right_arrow_visible"] = True
 
-    def menu_item(icon: str, label: str, action: Callable):
-        with ui.item().props("clickable ripple").classes("p-5 pl-7 bg-gray-200").on('click', lambda: router.open(action)):
+    def left_drawer_open():
+        left_drawer.props("mini=False")
+        state["left_drawer_left_arrow_visible"] = True
+        state["left_drawer_right_arrow_visible"] = False
+
+    def menu_item(key: str, icon: str, label: str, action: Callable):
+        with (
+            ui.item()
+            .props("clickable ripple")
+            .classes("p-3 pl-7 mb-2 text-gre")
+            .on("click", lambda: open_page(key, action))
+        ) as item:
+
+            def on_click():
+                state["active_page"] = key
+                for _key, data in state["menu_items"].items():
+                    if data["object"]:
+                        data["object"].classes(
+                            "bg-gray-300" if _key == key else "",
+                            remove="bg-gray-300" if _key != key else "",
+                        )
+
+            item.on(
+                "click",
+                on_click,
+            )
             with ui.item_section().props("avatar").style("min-width: 0;"):
                 ui.icon(icon)
-            ui.item_section(label).bind_visibility_from(left_drawer.props, 'mini')
+            ui.item_section(label).bind_visibility_from(left_drawer.props, "mini")
+        return item
 
-    @router.add('/')
+    def open_page(key, func):
+        state["active_page"] = key
+        router.open(func)
+
+    @router.add("/")
     def show_one():
-        ui.markdown('## 🏠 Home')
-        ui.label('Content One').classes('text-2xl')
-        ui.label('This is the main content area.').classes('text-lg')
+        ui.markdown("## 🏠 Home")
+        ui.label("Content One").classes("text-2xl")
+        ui.label("This is the main content area.").classes("text-lg")
 
-
-    @router.add('/two')
+    @router.add("/two")
     def show_two():
         ui.markdown("# 🏠 Home")
-        ui.label('Content Two').classes('text-2xl')
+        ui.label("Content Two").classes("text-2xl")
 
-    @router.add('/three')
+    @router.add("/three")
     def show_three():
-        ui.label('Content Three').classes('text-2xl')
+        ui.label("Content Three").classes("text-2xl")
 
-    with ui.left_drawer(top_corner=True, bottom_corner=True).props('mini=False').classes("p-0 bg-gray-100") as left_drawer:
-        left_drawer.bind_value_to(state, 'props', lambda v: print(v))
-        with ui.element('div').classes('p-0 w-full nicegui-header').style('height: 68px;'):
-            with ui.item().props("clickable").classes("p-5 text-3xl w-full h-full").on('click', lambda: router.open(show_one)):
+    state["menu_items"] = {
+        "dashboard": {
+            "show": show_one,
+            "object": None,
+            "label": "Dashboard",
+            "icon": "dashboard",
+        },
+        "trading": {
+            "show": show_one,
+            "object": None,
+            "label": "Trading",
+            "icon": "candlestick_chart",
+        },
+        "analytics": {
+            "show": show_two,
+            "object": None,
+            "label": "Analytics",
+            "icon": "analytics",
+        },
+        "journal": {
+            "show": show_three,
+            "object": None,
+            "label": "Journal",
+            "icon": "description",
+        },
+        "accounts": {
+            "show": show_three,
+            "object": None,
+            "label": "Accounts",
+            "icon": "business_center",
+        },
+    }
+
+    with (
+        ui.left_drawer(top_corner=True, bottom_corner=True)
+        .props("bordered")
+        .classes("p-0 gap-3 bg-gray-100") as left_drawer
+    ):
+        with (
+            ui.element("div")
+            .classes("p-0 w-full nicegui-header mt-3")
+            .style("height: 68px;")
+        ):
+            with (
+                ui.item()
+                .props("clickable")
+                .classes("p-5 text-3xl w-full h-full")
+                .on("click", lambda: router.open(show_one))
+            ):
                 with ui.item_section().props("avatar").style("min-width: 0;"):
-                    ui.icon('analytics',size='1.5em')
-                ui.item_section("Trading App").bind_visibility_from(left_drawer, 'mini')
+                    ui.icon("analytics", size="1.5em")
+                ui.item_section("Trading App").bind_visibility_from(left_drawer, "mini")
 
         with ui.list().classes("text-xl").classes("w-full justify-start items-start"):
-            menu_item('home', 'Home', show_one)
-            menu_item('send', 'Send', show_two)
-            menu_item('analytics', 'Analytics', show_three)
+            for key, data in state["menu_items"].items():
+                state["menu_items"][key]["object"] = menu_item(
+                    key, data["icon"], data["label"], data["show"]
+                )
 
-    with ui.right_drawer(top_corner=True, bottom_corner=True).style('background-color: #ebf1fa').props('bordered') as right_drawer:
-        ui.label('RIGHT DRAWER')
+    with (
+        ui.right_drawer(top_corner=True, bottom_corner=True)
+        .style("background-color: #ebf1fa")
+        .props("bordered") as right_drawer
+    ):
+        ui.label("RIGHT DRAWER")
 
-    with ui.header().classes('items-center justify-between bg-transparent'):
-        with ui.element('div').classes('flex items-center gap-2'):
-            ui.button(on_click=lambda: toggle_left_drawer, icon='keyboard_double_arrow_left').props('flat').classes("text-black").bind_visibility_from(left_drawer.props, 'mini')
-            ui.button(on_click=lambda: toggle_left_drawer, icon='keyboard_double_arrow_right').props('flat').classes("text-black").bind_visibility_from(left_drawer.props, 'mini')
-            print(left_drawer.props)
-            ui.label(f"{state['props']}").classes('text-lg font-bold text-black')
+    with ui.header().classes("items-center justify-between bg-transparent"):
+        with ui.element("div").classes("flex items-center gap-2"):
+            ui.button(
+                on_click=left_drawer_collapse, icon="keyboard_double_arrow_left"
+            ).props("flat").classes("text-grey-6").bind_visibility_from(
+                state, "left_drawer_left_arrow_visible"
+            )
+            ui.button(
+                on_click=left_drawer_open, icon="keyboard_double_arrow_right"
+            ).props("flat").classes("text-grey-6").bind_visibility_from(
+                state, "left_drawer_right_arrow_visible"
+            )
 
-
-
-    router.frame().classes('w-full p-5 pt-0')
+    router.frame().classes("w-full p-5 pt-0 gap-0")
 
 
 ui.run(port=8123)
